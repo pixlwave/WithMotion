772f1b54f4a365c0f2e2c2164083b4c2518839fc
Justin McPherson <justin@threewaters.net>
2015-02-24 22:11:56 +1000

    Use introspection to discover init methods.

--- lib/WithMotion/nsobject_with.rb
+++ lib/WithMotion/nsobject_with.rb
@@ -11,7 +11,28 @@ class NSObject
       objc_args << a[1]
     end
 
-    self.alloc.send(method, *objc_args)
+    method_signature = instanceMethodSignatureForSelector(method)
+    if method_signature.nil?
+      raise NoMethodError, "with - invalid init method #{method}"
+    end
+
+    if method_signature.numberOfArguments - 2 != objc_args.length
+      raise ArgumentError, "with - invalid number of arguments: #{objc_args.length} for #{method_signature.numberOfArguments - 2}"
+    end
+
+    rv = Pointer.new(:object)
+    NSInvocation.invocationWithMethodSignature(method_signature).tap do |invocation|
+      invocation.setSelector(method)
+      objc_args.each_index do |i|
+        p = Pointer.new(method_signature.getArgumentTypeAtIndex(i + 2))
+        p[0] = objc_args[i]
+        invocation.setArgument(p, atIndex:i + 2)
+      end
+      invocation.invokeWithTarget(self.alloc)
+      invocation.getReturnValue(rv)
+    end
+    rv[0]
   end
-  
+
 end
+
